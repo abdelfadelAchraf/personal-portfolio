afd79622767310dab64b860ba5aac43b9d3c2441
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-17 21:03:57 +0100

    add the skills section/component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
+import Skills from './components/skills'
 const App = () => {
   return (
     <main className='overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased h-screen'>
@@ -10,7 +11,8 @@ const App = () => {
       <Home/>
       <About/>
       <Projects/>
-
+      <Skills/>
+      
     
     </main>
   )
---
+++ src/components/Skills.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { SKILLS } from '../constants'
+
+const Skills = () => {
+  return (
+    <section className="container mx-auto" id='skills'>
+        <h2 className='mb-12 mt-20 text-center text-4xl'>Skills</h2>
+        <section className='mx-2 pt-2 flex flex-col rounded-xl bg-gradient-to-b from-zinc-900 to-zinc-950 px-4 lg:px-20'>
+          {
+            SKILLS.map((skill, index) =>(
+               <section key={index} className='mb-8 flex items-center justify-between'>
+                <section className='flex items-center'>
+                    {skill.icon}
+                    <h3 className='px-6 text-xl lg:text-3xl'>{skill.name}</h3>
+                </section>
+
+                <section className='text-md border-b-2 border-yellow-400 font-semibold lg:text-xl'>
+                    <span>{skill.experience}</span>
+                </section>
+               </section>
+            ))
+          }
+        </section>
+    </section>
+  )
+}
+
+export default Skills
