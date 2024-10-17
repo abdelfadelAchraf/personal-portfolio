1cbc1c53509458b97abf497815d860e11a21c119
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-17 15:57:55 +0100

    add the home page component

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PROFILE } from '../constants'
+
+const Home = () => {
+  return (
+    <section className='relative w-screen h-screen bg-red-500'>
+        
+    </section>
+  )
+}
+
+export default Home
