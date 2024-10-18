de5a4ac1fc015bbe0e5e70fde88d3300fd70a2fe
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-18 18:42:43 +0100

    add testimonials section animation

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { TESTIMONIALS } from '../constants'
-
+import { motion } from 'framer-motion'
 const Testimonials = () => {
   return (
    <section id='testimonials' className='container mx-auto mt-20 py-16 tracking-tighter'>
-       <h2 className='mb-12 text-center text-4xl font-semibold'>What people say ?</h2>
+       <motion.h2 className='mb-12 text-center text-4xl font-semibold'
+          initial={{opacity:0, x:-100  }}
+          whileInView={{opacity:1, x:0 }}
+          transition={{duration:1, delay:0.5}}
+       >What people say ?</motion.h2>
        <section className='mx-auto max-w-3xl'>
            {TESTIMONIALS.map((testimonial, index) =>(
-            <section key={index} className='mx-4 mb-8 flex flex-col items-center rounded-lg border border-dotted border-gray-600 p-6 md:flex-row'>
+            <motion.section key={index} className='mx-4 mb-8 flex flex-col items-center rounded-lg border border-dotted border-gray-600 p-6 md:flex-row'
+            initial={{opacity:0, y:-100  }}
+            whileInView={{opacity:1, y:0 }}
+            transition={{duration:1, delay:0.5}}
+            >
                 <img src={testimonial.image} alt={testimonial.name} className='mb-4 mr-6 h-16 w-16 rounded-full md:mb-0' />
               
               <section>
@@ -15,7 +23,7 @@ const Testimonials = () => {
               <p className='font-bold'>{testimonial.name}</p>
               <p className='text-gray-400'>{testimonial.title}</p>
               </section>
-            </section>
+            </motion.section>
           
            ))}
        </section>
