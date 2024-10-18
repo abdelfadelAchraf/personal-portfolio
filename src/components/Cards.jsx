91f216d88deaf59ce9e89b8b9b09300802408a18
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-18 15:15:53 +0100

    add hover effect  to cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 const Cards = ({image,title , subtitle,link}) => {
   return (
     <a href={link} className="m-4 block max-w-sm overflow-hidden rounded-lg">
-        <section className="relative ">
+        <section className="relative hover:scale-105 transition-all ">
             <img src={image} className="w-full" alt={title} />
             <section className="flex flex-col justify-between p-4 text-white">
                 <h2 className="mb-2 text-2xl font-bold">{title}</h2>
