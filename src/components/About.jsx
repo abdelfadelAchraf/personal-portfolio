36b0b22b3423d02e4fc0da53184337c5c0d881a7
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-18 16:29:49 +0100

    Add anmiation to  the About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { ABOUT } from '../constants'
-
+import { motion } from 'framer-motion'
 const About = () => {
   return (
     <section className='container mx-auto' id='about'> 
-         <h2 className='mt-20 text-center text-4xl font-semibold'>About</h2>
-         <h3 className='p-4 text-6xl uppercase font-semibold lg:text-[8rem]'>{ABOUT.text1}</h3>
-         <p className='mr-24 pl-4 leading-loose'>{ABOUT.text2}</p>
+         <motion.h2 className='mt-20 text-center text-4xl font-semibold'
+         initial={{opacity:0, y:-20  }}
+         whileInView={{opacity:1, y:0 }}
+         transition={{duration:.5, delay:.2}}
+         viewport={{once:true}}
+         >About</motion.h2>
+         <motion.h3 className='p-4 text-6xl uppercase font-semibold lg:text-[8rem]'
+          initial={{opacity:0, x:-20  }}
+          whileInView={{opacity:1, x:0 }}
+          transition={{duration:1,delay:.3}}
+          viewport={{once:true}}
+         >{ABOUT.text1}</motion.h3>
+         <motion.p className='mr-24 pl-4 leading-loose'
+          initial={{opacity:0, x:100  }}
+          whileInView={{opacity:1, x:0 }}
+          transition={{duration:1}}
+          viewport={{once:true}}
+         >{ABOUT.text2}</motion.p>
     </section>
   )
 }
