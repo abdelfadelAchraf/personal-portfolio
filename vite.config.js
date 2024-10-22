dab2d39170c3d87e1caee3453997ac15ae146dab
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2024-10-22 11:31:54 +0100

    Small modification

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/repository-name/', // replace 'repository-name' with your actual repository name
 })
